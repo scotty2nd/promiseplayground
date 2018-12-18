640e3aeafc30a70c54ca2f6816f4b952a30160b2
cwirsching <c.wirsching@neusta>
2018-12-18 13:41:35 +0100

    created initial register request

--- Pages/RegisterPage.js
+++ Pages/RegisterPage.js
@@ -2,6 +2,31 @@ function goBack() {
 	router.goBack();
 }
 
+function sendRequest() {
+	const newPost = {
+    	email: "sydney@fife",
+    	password: "123"
+	}
+
+	const options = {
+	    method: 'POST',
+	    body: JSON.stringify(newPost),
+	    headers: new Headers({
+	        'Content-Type': 'application/json'
+	    })
+	}
+
+	fetch('https://reqres.in/api/register', options)
+    	.then(function(response){
+    		return response.json();
+    	})
+		.then(function(data){
+			console.dir(data);
+    		return data;
+    	})
+}
+
 module.exports = {
-	goBack: goBack
+	goBack: goBack,
+	sendRequest: sendRequest
 };
